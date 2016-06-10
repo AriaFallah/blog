73a3b0796a48a5162397d28c1b04fe4fb879c7bb
AriaFallah <aria@fallah.us>
2016-06-10 03:00:29 -0400

    :bug: apparently I can't read

--- blog/renew_ssl.sh
+++ blog/renew_ssl.sh
@@ -6,6 +6,5 @@ compose=/usr/local/aria.ai/blog/docker-compose.yml
 
 certbot-auto renew \
   --standalone \
-  -d blog.aria.ai \
   --pre-hook "docker-compose -f ${compose} stop" \
   --post-hook "docker-compose -f ${compose} start"
